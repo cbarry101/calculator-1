1c6fbf69e1ef53cb3fb206acf500bdf1e2b5d145
Connor Barry <cbarry578@gmail.com>
2021-11-09 13:50:03 -0800

    Correctly fixed square root

--- calc.js
+++ calc.js
@@ -30,7 +30,7 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 / num2)
 	}
 	if (mathSymbol == "sqrt"){
-		console.log(Math.sqrt(num1))
+		console.log(Math.sqrt(num1), Math.sqrt(num2))
 	}
 
 	// This line closes the connection to the command line interface.
